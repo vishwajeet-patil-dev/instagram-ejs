51d7e29e425aab82af1f255332ee975641db47fb
Vishwajeet Patil <official.vishupatil@gmail.com>
2026-03-14 20:21:45 +0530

    Add new logo image to the public images directory

--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("pages/login");
+  res.render("pages/login", {
+    logo: "/images/logo.png",
+    appstore: "/images/appstore.png",
+    googleplay: "/images/googleplay.png",
+    error: null,
+    loading: false,
+  });
 });
 
 module.exports = router;
